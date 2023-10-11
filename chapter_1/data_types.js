cc4d9ffa1f6d057fda9d0e6008aeab4eb20eebf9
ReddSnake <horatiu.crisan01@e-uvt.ro>
2023-10-11 01:58:20 -0700

    First chapter of the tutorial

---
+++ chapter_1/data_types.js
@@ -0,0 +1,26 @@
+console.log(typeof "Dave")
+console.log(typeof 42)
+console.log(typeof true)
+
+//define a variable
+let myVariable
+console.log(typeof myVariable)
+
+myVariable = "Dave"
+console.log(typeof myVariable)
+
+myVariable = 42
+console.log(typeof myVariable)
+
+let myNumber = 10
+myVariable = "Dave"
+
+console.log( typeof (myVariable + myNumber))
+
+console.log(myVariable === myNumber)
+
+let myBool = myNumber === 10
+
+console.log(myBool)
+
+console.log(myNumber !== 10)
